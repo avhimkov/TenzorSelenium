1aff2828d9070f657804b39b8ac74af62d3c35c2
Dmitry <avhimkov@mail.ru>
2017-09-03 22:27:34 +0500

    refactoring TenzorQuery

--- src/main/java/org.adlsoft/TensorQuery.java
+++ src/main/java/org.adlsoft/TensorQuery.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.*;
 
-import org.openqa.selenium.support.ui.Select;
-
 public class TensorQuery {
 
     @Test
@@ -24,36 +22,17 @@ public class TensorQuery {
                 String basedURL = str[0];
                 driver.get(basedURL);
 
-                //mail.ru
+                //https://mail.ru/
 //                driver.findElement(By.id("mailbox__login")).clear();
 //                driver.findElement(By.id("mailbox__login")).sendKeys(str[1]);
 //                driver.findElement(By.id("mailbox__password")).clear();
 //                driver.findElement(By.id("mailbox__password")).sendKeys(str[2]);
 //                driver.findElement(By.id("mailbox__auth__button")).click();
-//                Thread.sleep(5000);
 
                 //https://uploadfiles.io/
-                driver.findElement(By.cssSelector("input.dz-hidden-input")).sendKeys(str[1]);
-                Thread.sleep(5000);
+//                driver.findElement(By.cssSelector("input.dz-hidden-input")).sendKeys(str[1]);
 
-//                        int fe_id = 0;
-//                        switch (fe_id) {
-//                            case 1:
-//                                driver.findElement(By.id("title_operator")).click();
-//                                break;
-//                            case 2:
-//                                driver.findElement(By.id("title_controller")).click();
-//                                break;
-//                            case 3:
-//                                driver.findElement(By.id("title_docved")).click();
-//                                break;
-//                            case 4:
-//                                driver.findElement(By.id("title_esia")).click();
-//                                break;
-//                            default:
-//                                break;
-//                        }
-
+                Thread.sleep(3000);
             }
         }
         driver.quit();
