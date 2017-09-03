99169d7a3791182a9d99c4e4822e804377e6121a
Dmitry <avhimkov@mail.ru>
2017-09-03 20:25:01 +0500

    refactoring Main class

--- src/test/java/org/adlsoft/Main.java
+++ src/test/java/org/adlsoft/Main.java
@@ -1,5 +1,20 @@
 package java.org.adlsoft;
 
-public class Main {
+import org.junit.internal.TextListener;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
 
+public class Main {
+    public static void main(String args[]) {
+        JUnitCore junit = new JUnitCore();
+        junit.addListener(new TextListener(System.out));
+        Result result = junit.run(TensorQuery.class); // Replace "SampleTest" with the name of your class
+        if (result.getFailureCount() > 0) {
+            System.out.println("Test failed.");
+            System.exit(1);
+        } else {
+            System.out.println("Test finished successfully.");
+            System.exit(0);
+        }
+    }
 }
