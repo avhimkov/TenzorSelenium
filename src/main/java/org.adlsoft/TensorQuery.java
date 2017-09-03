b1be28307bad66c40875707075c55476fbfdbaf4
Dmitry <avhimkov@mail.ru>
2017-09-04 00:49:53 +0500

    add io.github.bonigarcia maven dependency and more dependency and refactoring WebDrivers in project

--- src/main/java/org.adlsoft/TensorQuery.java
+++ src/main/java/org.adlsoft/TensorQuery.java
@@ -1,5 +1,6 @@
 package org.adlsoft;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,7 +12,8 @@ public class TensorQuery {
     @Test
     public void testCase() throws InterruptedException, IOException {
         String lineLogin;
-        System.setProperty("webdriver.chrome.driver", "driver/win/x32/chromedriver.exe");
+        ChromeDriverManager.getInstance().setup();
+//        System.setProperty("webdriver.chrome.driver", "driver/win/x32/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("csv/login.csv"), "windows-1251"));
@@ -23,16 +25,16 @@ public class TensorQuery {
                 driver.get(basedURL);
 
                 //https://mail.ru/
-//                driver.findElement(By.id("mailbox__login")).clear();
-//                driver.findElement(By.id("mailbox__login")).sendKeys(str[1]);
-//                driver.findElement(By.id("mailbox__password")).clear();
-//                driver.findElement(By.id("mailbox__password")).sendKeys(str[2]);
-//                driver.findElement(By.id("mailbox__auth__button")).click();
+                driver.findElement(By.id("mailbox__login")).clear();
+                driver.findElement(By.id("mailbox__login")).sendKeys(str[1]);
+                driver.findElement(By.id("mailbox__password")).clear();
+                driver.findElement(By.id("mailbox__password")).sendKeys(str[2]);
+                driver.findElement(By.id("mailbox__auth__button")).click();
 
                 //https://uploadfiles.io/
 //                driver.findElement(By.cssSelector("input.dz-hidden-input")).sendKeys(str[1]);
 
-                Thread.sleep(3000);
+//                Thread.sleep(2000);
             }
         }
         driver.quit();
