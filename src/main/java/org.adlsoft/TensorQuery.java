754ae9cf0c0d6603e00470ccc969d60a92a1ccbc
Dmitry <avhimkov@mail.ru>
2017-09-04 11:37:33 +0500

    add method captureScreenshot

--- src/main/java/org.adlsoft/TensorQuery.java
+++ src/main/java/org.adlsoft/TensorQuery.java
@@ -1,11 +1,20 @@
 package org.adlsoft;
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.apache.commons.io.FileUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import java.io.*;
 
 public class TensorQuery {
@@ -17,7 +26,6 @@ public class TensorQuery {
 //        PhantomJsDriverManager.getInstance().setup();
 //        EdgeDriverManager.getInstance().setup();
 //        InternetExplorerDriverManager.getInstance().setup();
-
 
         ChromeDriverManager.getInstance().setup();
     }
@@ -41,12 +49,30 @@ public class TensorQuery {
                 driver.findElement(By.id("mailbox__password")).sendKeys(str[2]);
                 driver.findElement(By.id("mailbox__auth__button")).click();
 
+                String screenpath = captureScreenshot(driver, str[1]);
                 //https://uploadfiles.io/
 //                driver.findElement(By.cssSelector("input.dz-hidden-input")).sendKeys(str[1]);
 
-//                Thread.sleep(2000);
+                Thread.sleep(2000);
             }
         }
         driver.quit();
     }
+
+
+
+    public static String captureScreenshot (WebDriver driver, String screenshotName){
+
+        try {
+            TakesScreenshot ts = (TakesScreenshot)driver;
+            File source = ts.getScreenshotAs(OutputType.FILE);
+            String dest = "d:/" + screenshotName + ".png";
+            File destination = new File(dest);
+            FileUtils.copyFile(source, destination);
+            return dest;
+        }
+
+        catch (IOException e) {return e.getMessage();}
+    }
+
 }
